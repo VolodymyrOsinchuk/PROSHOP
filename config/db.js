1c2340d5f64f37cea7c364f9bdece4c08698e299
Volo <osinchuk28@live.com>
2021-12-02 17:33:13 +0100

    update db

--- config/db.js
+++ config/db.js
@@ -6,9 +6,8 @@ dotenv.config();
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-       useNewUrlParser: true ,
-       useUnifiedTopology: true,
-       useCreateIndex: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
